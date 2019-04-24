46ab6953b77f9609bc12abf51a903ba69d11c6af
Marcial <marcial.a.villarreal.p@gmail.com>
2019-04-24 00:11:07 -0300

    primer commit

---
+++ src/main/java/com/example/demoRules/demoRules/DemoRulesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoRules.demoRules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRulesApplication.class, args);
+	}
+
+}
